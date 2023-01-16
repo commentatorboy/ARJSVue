0ec2cca219b8b3de62d85ff3de81c4fb82484080
Hamun <hf@ordrestyring.dk>
2023-01-16 21:57:43 +0100

    first

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+import "@ar-js-org/ar.js"
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/views/MyARProjectsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>My AR projects</h1>
+
+  </div>
+
+  <a-scene embedded arjs>
+      <a-marker preset="hiro">
+          <!-- we use cors proxy to avoid cross-origin problems ATTENTION! you need to set up your server -->
+          <a-entity
+          position="0 0 0"
+          scale="0.05 0.05 0.05"
+          gltf-model="./src/assets/scene.gltf"
+          ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
+      </a-scene>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+
+body {
+  margin : 0px; overflow: hidden;
+}
+</style>
